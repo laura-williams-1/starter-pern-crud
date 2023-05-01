abdcb86b3f58a2a4cc344ac4772d5531a6be35c9
laura-williams-1 <laurawilliams@pursuit.org>
2023-05-01 18:30:30 -0400

    created state and state function for variavle reviews

--- src/Components/Reviews.js
+++ src/Components/Reviews.js
@@ -5,6 +5,8 @@ import { useParams } from "react-router-dom";
 const API = process.env.REACT_APP_API_URL;
 
 function Reviews() {
+  const [reviews, setReviews] = useState([]);
+
   return <div></div>;
 }
 export default Reviews;
